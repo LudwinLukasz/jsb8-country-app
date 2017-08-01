a66004d22bb602163d8cdc80eb1f6a13d57d0a63
Lukasz Ludwin <gorli@interia.pl>
2017-08-02 00:54:26 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+//import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import {getCountries} from './actions/actions-countries';
-import DevTools from './DevTools';
+//import {getCountries} from './actions/actions-countries';
+//import DevTools from './DevTools';
+//import Navigation from './presentational/navigation.component';
+import routes from './routes';
 
 render(
-    <Provider store={store}>
-        <DevTools />
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		 <Router history={hashHistory} routes={routes}/>
+	</Provider>,
+	document.getElementById('root')
 );
-store.dispatch(getCountries());
+//store.dispatch(getCountries());
